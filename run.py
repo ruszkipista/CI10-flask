4c18b8456a0ee945728e3e408abd89b3ee744e87
ruszkipista <oi5672@gmail.com>
2021-04-08 22:23:48 +0100

    add 2 template html files

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
+# name of the application module or package (="__main__")
+# where should Flask look for templates and static files
+app = Flask(__name__) 
 
-app = Flask(__name__)
-
-
-@app.route("/")
+@app.route("/")  # trigger point through webserver: "/"= root directory
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
-
+# script runs as main, not as imported code
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "127.0.0.1"),
-        port=int(os.environ.get("PORT", "8080")),
-        debug=True)
+        host=os.environ.get("IP", "127.0.0.1"),  #get value or use given default
+        port=int(os.environ.get("PORT", "8080")),#get value or use given default
+        debug=True)         # allow debugging, only for development phase
