32f08faa6968a0c1ec8d5a9d84abb6cfc6133097
ruszkipista <oi5672@gmail.com>
2021-04-09 22:05:55 +0100

    fill About page with content from json file

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 # name of the application module or package (="__main__")
@@ -11,15 +12,17 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    with open("./data/company.json", "r", encoding="UTF-8") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")    
+    return render_template("careers.html", page_title="Careers")    
 
 # script runs as main, not as imported code
 if __name__ == "__main__":
