360c23b328bc790be645836d733e7832fe05285d
ruszkipista <oi5672@gmail.com>
2021-04-10 16:04:02 +0100

    add member page under about page

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 
 # name of the application module or package (="__main__")
 # where should Flask look for templates and static files
-app = Flask(__name__) 
+app = Flask(__name__)
 
 @app.route("/")  # trigger point through webserver: "/"= root directory
 def index():
@@ -14,7 +14,14 @@ def index():
 def about():
     with open("./data/company.json", "r", encoding="UTF-8") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", company=data)
+    return render_template("about.html", page_title="About", page_url='about', company=data)
+
+@app.route("/about/<member_url>")
+def member(member_url):
+    with open("./data/company.json", "r", encoding="UTF-8") as json_data:
+        data = json.load(json_data)
+        member_data = list(filter(lambda x: x['url']==member_url, data))
+    return render_template("member.html", member=member_data[0])
 
 @app.route("/contact")
 def contact():
@@ -28,5 +35,5 @@ def careers():
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "127.0.0.1"),  #get value or use given default
-        port=int(os.environ.get("PORT", "8080")),#get value or use given default
+        port=int(os.environ.get("PORT", "5500")),#get value or use given default
         debug=True)         # allow debugging, only for development phase
